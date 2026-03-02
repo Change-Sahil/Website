696d0a36f815c0dda5d043a3f1141a826ecc33eb
Seref Sahil <seref@sahilmail.de>
2026-03-02 14:13:04 +0100

    Redirect .com to English homepage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,39 @@
+// next.config.js
 const withNextIntl = require("next-intl/plugin")("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      // .com -> englische Startseite auf .de
+      {
+        source: "/",
+        has: [{ type: "host", value: "change-werkstatt-sahil.com" }],
+        destination: "https://change-werkstatt-sahil.de/en",
+        permanent: true, // 308
+      },
+      // optional: www.com -> englische Startseite auf .de
+      {
+        source: "/",
+        has: [{ type: "host", value: "www.change-werkstatt-sahil.com" }],
+        destination: "https://change-werkstatt-sahil.de/en",
+        permanent: true, // 308
+      },
+      // optional: alle Unterpfade auf .com ebenfalls nach .de spiegeln (empfohlen)
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "change-werkstatt-sahil.com" }],
+        destination: "https://change-werkstatt-sahil.de/en/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.change-werkstatt-sahil.com" }],
+        destination: "https://change-werkstatt-sahil.de/en/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
